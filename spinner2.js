3a0a92e34a11f5d157e0ca41c42bea800e44dea1
Ina Ricci <ina_ricci@hotmail.com>
2023-12-23 09:52:18 -0800

    Refactored code for spinner1.js

---
+++ spinner2.js
@@ -0,0 +1,8 @@
+const characters = ['|', '/', '-', '\\', '|', '/', '-', '|'];
+let delay = 100;
+for (let i = 0; i < characters.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(`\r${characters[i]}   `);
+  }, delay);
+  delay += 200;
+}
